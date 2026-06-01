8af80e2802e76c00692df9ee3786dafcb0f26433
Theo <theoddsouza@gmail.com>
2026-06-01 10:37:09 -0300

    Refatore os métodos listar nos controladores AutorController e FilmeController para incluir filtros por nome e data de lançamento, melhorando o tratamento de erros.

--- ApiFilme/app/Http/Controllers/AutorController.php
+++ ApiFilme/app/Http/Controllers/AutorController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Filme;
 use App\Models\Autor;
 
@@ -8,9 +9,30 @@ use Illuminate\Http\Request;
 
 class AutorController extends Controller
 {
-    public function listar(){
-        $query = Autor::query();
-        $Autores = $query->get();
-        return view('ListarAutor', compact('Autores'));
+    public function listar(Request $request){
+        try {
+            $query = Autor::query();
+
+            // Filtro por nome
+            // Select * from autores where nome like %VAR%
+            if ($request->filled('nome')) {
+                $query->where('nome', 'like', '%' . $request->nome . '%');
+            }
+
+            // Filtro por telefone
+            if ($request->filled('telefone')) {
+                $query->where('telefone', $request->telefone);
+            }
+
+            $Autores = $query->get();
+
+            return view('ListarAutor', compact('Autores'));
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => "Erro interno do servidor",
+                'errors' => $e->getMessage()
+            ], 500);
+        }
     }
 }
--- ApiFilme/app/Http/Controllers/FilmeController.php
+++ ApiFilme/app/Http/Controllers/FilmeController.php
@@ -8,10 +8,31 @@ use Illuminate\Http\Request;
 
 class FilmeController extends Controller
 {
-    public function listar(){
-        $query = Filme::query();
-        $Filmes = $query->get();
-        return view('ListarFilme', compact('Filmes'));
+    public function listar(Request $request){
+        try {
+            $query = Filme::query();
+
+            // Filtro por nome
+            // Select * from autores where nome like %VAR%
+            if ($request->filled('titulo')) {
+                $query->where('titulo', 'like', '%' . $request->titulo . '%');
+            }
+
+            // Filtro por telefone
+            if ($request->filled('dataLancamento')) {
+                $query->where('dataLancamento', $request->dataLancamento);
+            }
+
+            $Filmes = $query->get();
+
+            return view('ListarFilme', compact('Filmes'));
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => "Erro interno do servidor",
+                'errors' => $e->getMessage()
+            ], 500);
+        }
     }
 
 }
